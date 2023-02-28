dd42156f87a9253aebb7533063eda7a519b29ab3
Andres Almiray <aalmiray@gmail.com>
2023-02-28 17:54:54 +0100

    feat(core): Simplify how tool version is read

--- pomchecker-cli/src/main/java/org/kordamp/maven/checker/cli/Versions.java
+++ pomchecker-cli/src/main/java/org/kordamp/maven/checker/cli/Versions.java
@@ -17,91 +17,21 @@
  */
 package org.kordamp.maven.checker.cli;
 
+import org.kordamp.maven.checker.PomcheckerVersion;
 import picocli.CommandLine;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.ResourceBundle;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 /**
  * @author Andres Almiray
  * @since 1.1.0
  */
 public class Versions implements CommandLine.IVersionProvider {
-    private static final ResourceBundle bundle = ResourceBundle.getBundle(Versions.class.getName());
-    private static final String POMCHECKER_VERSION = bundle.getString("pomchecker_version");
-    private static final String SEPARATOR = "------------------------------------------------------------%n";
-
     @Override
     public String[] getVersion() throws Exception {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        banner(new PrintStream(baos));
+        PomcheckerVersion.banner(new PrintStream(baos));
         return baos.toString().split("\n");
-    }
-
-    public static void banner(PrintStream out) {
-        Manifest manifest = findMyManifest();
-        if (null != manifest) {
-            String version = manifest.getMainAttributes().getValue(Attributes.Name.IMPLEMENTATION_VERSION);
-            String buildTimestamp = manifest.getMainAttributes().getValue("Build-Timestamp");
-            String buildRevision = manifest.getMainAttributes().getValue("Build-Revision");
-            boolean additionalInfo = isNotBlank(buildTimestamp) || isNotBlank(buildRevision);
-
-            out.printf(SEPARATOR);
-            out.printf("PomChecker %s%n", version);
-            out.printf(SEPARATOR);
-            if (additionalInfo) {
-                if (isNotBlank(buildTimestamp)) {
-                    out.printf("Build timestamp: %s%n", buildTimestamp);
-                }
-                if (isNotBlank(buildRevision)) out.printf("Revision:        %s%n", buildRevision);
-                out.printf(SEPARATOR);
-            }
-        } else {
-            out.printf("%s%n", POMCHECKER_VERSION);
-        }
-    }
-
-    private static Manifest findMyManifest() {
-        try {
-            Enumeration<URL> urls = Versions.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
-            while (urls.hasMoreElements()) {
-                URL url = urls.nextElement();
-                Manifest manifest = new Manifest(url.openStream());
-                if (manifest.getMainAttributes().containsKey(Attributes.Name.IMPLEMENTATION_TITLE)) {
-                    String specificationTitle = manifest.getMainAttributes().getValue(Attributes.Name.IMPLEMENTATION_TITLE);
-                    if ("pomchecker-cli".equals(specificationTitle)) {
-                        return manifest;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            // well, this sucks
-        }
-
-        return null;
-    }
-
-    public static boolean isBlank(String str) {
-        if (str == null || str.length() == 0) {
-            return true;
-        }
-
-        for (char c : str.toCharArray()) {
-            if (!Character.isWhitespace(c)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean isNotBlank(String str) {
-        return !isBlank(str);
     }
 }
---
+++ pomchecker-core/src/main/java/org/kordamp/maven/checker/PomcheckerVersion.java
@@ -0,0 +1,83 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020-2023 Andres Almiray.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kordamp.maven.checker;
+
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.ResourceBundle;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+/**
+ * @author Andres Almiray
+ * @since 1.9.0
+ */
+public class PomcheckerVersion {
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(PomcheckerVersion.class.getName());
+    private static final String JRELEASER_VERSION = BUNDLE.getString("pomchecker_version");
+    private static final String BUILD_TIMESTAMP = BUNDLE.getString("build_timestamp");
+    private static final String BUILD_REVISION = BUNDLE.getString("build_revision");
+    private static final String SEPARATOR = "------------------------------------------------------------%n";
+    private static final String POMCHECKER_VERSION_FORMATTED = "pomchecker %s%n";
+
+    public static String getPlainVersion() {
+        return JRELEASER_VERSION;
+    }
+
+    public static void banner(PrintStream out) {
+        banner(out, true);
+    }
+
+    public static void banner(PrintStream out, boolean full) {
+        banner(newPrintWriter(out), full);
+    }
+
+    public static void banner(PrintWriter out) {
+        banner(out, true);
+    }
+
+    public static void banner(PrintWriter out, boolean full) {
+        if (full) {
+            out.printf(SEPARATOR);
+            out.printf(POMCHECKER_VERSION_FORMATTED, JRELEASER_VERSION);
+
+            String jvm = System.getProperty("java.version") + " (" +
+                System.getProperty("java.vendor") + " " +
+                System.getProperty("java.vm.version") + ")";
+
+            out.printf(SEPARATOR);
+            out.printf("Build timestamp: %s%n", BUILD_TIMESTAMP);
+            out.printf("Revision:        %s%n", BUILD_REVISION);
+            out.printf("JVM:             %s%n", jvm);
+            out.printf(SEPARATOR);
+        } else {
+            out.printf(POMCHECKER_VERSION_FORMATTED, JRELEASER_VERSION);
+        }
+    }
+
+    private static PrintWriter newPrintWriter(OutputStream out) {
+        return newPrintWriter(out, true);
+    }
+
+    private static PrintWriter newPrintWriter(OutputStream out, boolean autoFlush) {
+        return new PrintWriter(new BufferedWriter(new OutputStreamWriter(out, UTF_8)), autoFlush);
+    }
+}
