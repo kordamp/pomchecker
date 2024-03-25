84c55a3bddea842fad9c5f215aaa1507624f21a6
Andres Almiray <aalmiray@gmail.com>
2024-03-25 10:41:07 +0100

    build: Update license headers

--- pomchecker-toolprovider/src/java9/module-info.java
+++ pomchecker-toolprovider/src/java9/module-info.java
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright 2020-2023 Andres Almiray.
+ * Copyright 2020-2024 Andres Almiray.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @author Andres Almiray
  * @since 1.7.0
