90978d5f1d51302e378954b29bce7311fe262fb0
Tamas Cservenak <tamas@cservenak.net>
2024-05-02 21:56:11 +0200

    Fix c-p pkg

--- pomchecker-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/checker/internal/PomParser.java
+++ pomchecker-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/checker/internal/PomParser.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kordamp.maven.checker.cli.internal;
+package org.kordamp.gradle.plugin.checker.internal;
 
 import com.google.common.base.CharMatcher;
 import com.google.common.collect.ImmutableMap;
