1cada16ad7db0b16b6118985d2e2b1a485bc9292
Tamas Cservenak <tamas@cservenak.net>
2024-09-04 23:02:16 +0200

    Update gradle one but still fails

--- pomchecker-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/checker/internal/PomParser.java
+++ pomchecker-gradle-plugin/src/main/groovy/org/kordamp/gradle/plugin/checker/internal/PomParser.java
@@ -17,24 +17,30 @@
  */
 package org.kordamp.gradle.plugin.checker.internal;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.collect.ImmutableMap;
 import eu.maveniverse.maven.mima.context.Context;
 import eu.maveniverse.maven.mima.context.ContextOverrides;
 import eu.maveniverse.maven.mima.context.Runtimes;
 import eu.maveniverse.maven.mima.extensions.mmr.MavenModelReader;
-import eu.maveniverse.maven.mima.extensions.mmr.ModelLevel;
+import eu.maveniverse.maven.mima.extensions.mmr.ModelRequest;
 import eu.maveniverse.maven.mima.extensions.mmr.ModelResponse;
-import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.repository.RepositoryPolicy;
 import org.eclipse.aether.resolution.ArtifactDescriptorException;
-import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.VersionResolutionException;
 import org.kordamp.maven.checker.MavenProject;
 
 import java.io.File;
-import java.util.Arrays;
 
 import static java.util.Objects.requireNonNull;
+
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * This class seems 100% copy from pomchecker-core? wth
@@ -50,13 +56,16 @@ public class PomParser {
      * (parent, BOMs imported, etc) MUST BE RESOLVABLE. Hence, there is possibility to provide "extra" remote
      * repositories.
      */
-    public static MavenProject createMavenProject(File pomFile, RemoteRepository... repositories) {
+    public static MavenProject createMavenProject(File pomFile, Collection<Path> extraRepositories) {
         requireNonNull(pomFile);
-        ContextOverrides.Builder contextOverridesBuilder = ContextOverrides.create().withUserSettings(true);
-        if (repositories.length > 0) {
+        ContextOverrides.Builder contextOverridesBuilder = ContextOverrides.create().withUserSettings(true).userProperties(detectOsProperties());
+        if (extraRepositories != null && !extraRepositories.isEmpty()) {
+            AtomicInteger counter = new AtomicInteger(0);
             contextOverridesBuilder
                     .addRepositoriesOp(ContextOverrides.AddRepositoriesOp.APPEND)
-                    .repositories(Arrays.asList(repositories));
+                    .repositories(extraRepositories.stream()
+                            .map(r -> toArtifactRepository("pomchecker_repository_" + (counter.incrementAndGet()), r.toUri().toString()))
+                            .collect(Collectors.toList()));
         }
         try (Context context = Runtimes.INSTANCE.getRuntime().create(contextOverridesBuilder.build())) {
             return createMavenProject(pomFile, context, new MavenModelReader(context));
@@ -66,12 +75,59 @@ public class PomParser {
     }
 
     private static MavenProject createMavenProject(File pomFile, Context context, MavenModelReader mavenModelReader) throws ArtifactResolutionException, VersionResolutionException, ArtifactDescriptorException {
-        ArtifactDescriptorRequest request = new ArtifactDescriptorRequest(new DefaultArtifact("irrelevant:irrelevant:irrelevant").setFile(pomFile), context.remoteRepositories(), "pomchecker");
+        ModelRequest request = ModelRequest.builder().setPomFile(pomFile.toPath()).setRequestContext("pomchecker").build();
         ModelResponse response = mavenModelReader.readModel(request);
         return new MavenProject(
                 pomFile,
-                response.toModel(ModelLevel.RAW),
-                response.toModel(ModelLevel.EFFECTIVE)
+                response.getRawModel(),
+                response.getEffectiveModel()
         );
+    }
+
+    private static final CharMatcher LOWER_ALPHA_NUMERIC =
+            CharMatcher.inRange('a', 'z').or(CharMatcher.inRange('0', '9'));
+
+    public static ImmutableMap<String, String> detectOsProperties() {
+        return ImmutableMap.of(
+                "os.detected.name",
+                osDetectedName(),
+                "os.detected.arch",
+                osDetectedArch(),
+                "os.detected.classifier",
+                osDetectedName() + "-" + osDetectedArch());
+    }
+
+    private static String osDetectedName() {
+        String osNameNormalized =
+                LOWER_ALPHA_NUMERIC.retainFrom(System.getProperty("os.name").toLowerCase(Locale.ENGLISH));
+
+        if (osNameNormalized.startsWith("macosx") || osNameNormalized.startsWith("osx")) {
+            return "osx";
+        } else if (osNameNormalized.startsWith("windows")) {
+            return "windows";
+        }
+        // Since we only load the dependency graph, not actually use the
+        // dependency, it doesn't matter a great deal which one we pick.
+        return "linux";
+    }
+
+    private static String osDetectedArch() {
+        String osArchNormalized =
+                LOWER_ALPHA_NUMERIC.retainFrom(System.getProperty("os.arch").toLowerCase(Locale.ENGLISH));
+        switch (osArchNormalized) {
+            case "x8664":
+            case "amd64":
+            case "ia32e":
+            case "em64t":
+            case "x64":
+                return "x86_64";
+            default:
+                return "x86_32";
+        }
+    }
+
+    private static RemoteRepository toArtifactRepository(String id, String url) {
+        RepositoryPolicy policy = new RepositoryPolicy(true, RepositoryPolicy.UPDATE_POLICY_NEVER, RepositoryPolicy.CHECKSUM_POLICY_WARN);
+        return new RemoteRepository.Builder(id, "default", url).setReleasePolicy(policy).setSnapshotPolicy(policy).build();
     }
 }
